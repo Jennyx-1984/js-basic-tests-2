ac166e1e8d7b3a41357e9db75565b5c87eeb3de3
Jennyx-1984 <163904901+Jennyx-1984@users.noreply.github.com>
2025-11-19 18:10:44 +0100

    finalizados los segundos testeos de javascript

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -4,6 +4,11 @@ describe("Variables en JS", () => {
     it("Completa todas las constantes que faltan", () => {
 
         const a = 10;
+        const b = 1.34;
+        const c = true;
+        const d = "Buenos días";
+        const e = "Pepe";
+        const f = "Buenos días, pepe";
 
         expect(a).toBe(10);
         expect(b).toBe(1.34);
@@ -15,18 +20,30 @@ describe("Variables en JS", () => {
     it("Completa todas las variables que faltan para que las operaciones resulten correctamente", () => {
 
         let a = 11;
+        let b = 0.34;
+        let c = 10;
+        let d = 10;
+        let e = 350;
+        let f = 10;
+        let g = 3;
+        let x = 20;
+        let y = 21;
 
         expect(a + b).toBe(11.34);
         expect(a * c).toBe(110);
         expect(d - e).toBe(-340);
         // investiga para qué sirven los operadores ** y %
-        expect(f ** g).toBe(1000);
-        expect(x % 2).toBe(0);
-        expect(y % 2).toBe(1);
+        expect(f ** g).toBe(1000); //exponencial, es decir, 10^3
+        expect(x % 2).toBe(0); //módulo, es decir, el resto de la división entre 2
+        expect(y % 2).toBe(1); //módulo, es decir, el resto de la división entre 2
     });
     it("Completa todas las variables que faltan para que se cumplan las condiciones", () => {
 
         let a = 10;
+        let b = 100;
+        let c = undefined;
+        let d = "Jenny";
+        let n = "Jennifer";
 
         expect(a > 9).toBeTruthy();
         expect(a < 11).toBeTruthy();
@@ -49,7 +66,7 @@ describe("condicionales en javascript", () => {
         };
 
         // substituye ??? por el valor que tiene la variable "result"        
-        expect(result).toBe("???");
+        expect(result).toBe("KO");
     });
     it("comparando cadenas (strings)", () => {
 
@@ -64,7 +81,7 @@ describe("condicionales en javascript", () => {
         };
 
         // substituye ??? por el valor que tiene la variable "result"        
-        expect(result).toBe("???");
+        expect(result).toBe("pikachu");
     });
     it("Interpolación de cadenas (strings) (1)", () => {
         let n = "Piña";
@@ -80,7 +97,7 @@ describe("condicionales en javascript", () => {
         };
 
         // substituye ??? por el valor que tiene la variable "result"        
-        expect(result).toBe("???");
+        expect(result).toBe("Pizza con Pepperoni");
     });
     it("Interpolación de cadenas (strings) (2)", () => {
         let n = "Piña";
@@ -96,7 +113,7 @@ describe("condicionales en javascript", () => {
         };
 
         // substituye ??? por el valor que tiene la variable "result"        
-        expect(result).toBe("???");
+        expect(result).toBe("Pizza con Piña");
     });
 });
 
@@ -104,24 +121,24 @@ describe("Funciones en JS", () => {
     it('¿Cuál es el resultado de invocar la función getNumber?', () => {
         let result = getNumber();
         // substituye "???" por el valor que tiene la variable "result"
-        expect(result).toBe("???");
+        expect(result).toBe(2);
     });
     it("¿Cuál es el resultado de invocar la función getResult?", () => {
         let result = getResult(10, 30, 2);
         // substituye "???" por el valor que tiene la variable "result"        
-        expect(result).toBe("???");
+        expect(result).toBe(28);
     });
     it("¿Cuál es el resultado de invocar la función getString?", () => {
         let result = getString("Cerebro", "Pinky");
         // substituye "???" por el valor que tiene la variable "result"        
-        expect(result).toBe("???");
+        expect(result).toBe("Son Pinky y Cerebro");
     });
     it("¿Cuál es el resultado de invocar la función getLength?", () => {
         // substituye "???" por el resultado en cada caso
-        expect(getLength("khaleesi mother of dragons breaker of chains")).toBe("???");
-        expect(getLength("sarah")).toBe("???");
-        expect(getLength("bob")).toBe("???");
-        expect(getLength("robertson")).toBe("???");
+        expect(getLength("khaleesi mother of dragons breaker of chains")).toBe("very long");
+        expect(getLength("sarah")).toBe("adecquate");
+        expect(getLength("bob")).toBe("too short");
+        expect(getLength("robertson")).toBe("long");
     });
 });
 
